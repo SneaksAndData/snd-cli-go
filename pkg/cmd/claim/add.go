12361e88e871aee23744ec223ddf573e68f8258d
adelinag08 <adma@ecco.com>
2024-02-20 13:09:54 +0100

    refactor claim

--- pkg/cmd/claim/add.go
+++ pkg/cmd/claim/add.go
@@ -2,41 +2,36 @@ package claim
 
 import (
 	"fmt"
-	"github.com/SneaksAndData/esd-services-api-client-go/claim"
 	"github.com/spf13/cobra"
-	"log"
 	"strings"
 )
 
 var ca []string
 
-func NewCmdAddClaim() *cobra.Command {
+func NewCmdAddClaim(service Service) *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "add",
 		Short: "Add a new claim to an existing user",
 		RunE: func(cmd *cobra.Command, args []string) error {
-
-			var claimService, err = InitClaimService(fmt.Sprintf(boxerClaimBaseURL, env))
-			if err != nil {
-				log.Fatal(err)
+			resp, err := addClaimRun(service, userId, claimProvider, ca)
+			if err == nil {
+				fmt.Println(resp)
 			}
-			return addClaimRun(claimService)
+			return err
 		},
 	}
 	cmd.Flags().StringSliceVarP(&ca, "claims", "c", []string{}, "Claims to add. e.g. snd add -c \"test1.test.sneaksanddata.com/.*:.*\" -c \"test2.test.sneaksanddata.com/.*:.*\"")
 	return cmd
 }
 
-func addClaimRun(claimService *claim.Service) error {
+func addClaimRun(claimService Service, userId, claimProvider string, ca []string) (string, error) {
 	// Add user claims
 	response, err := claimService.AddClaim(userId, claimProvider, ca)
 	if err != nil {
 		if strings.HasSuffix(err.Error(), "404") {
-			log.Fatalf("Failed to retrieve claims for user %s for claim provider %s : %v", userId, claimProvider, "User not found")
+			return "", fmt.Errorf("failed to add claims for user %s for claim provider %s : %v", userId, claimProvider, "User not found")
 		}
-		log.Fatalf("Failed to add claims for user %s with claim provider %s: %v", userId, claimProvider, err)
+		return "", fmt.Errorf("failed to add claims for user %s with claim provider %s: %v", userId, claimProvider, err)
 	}
-
-	fmt.Println("Response:", response)
-	return nil
+	return response, nil
 }
