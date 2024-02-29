a7c684fe547d8a0a008de3b7965c305484e46c82
adelinag08 <adma@ecco.com>
2024-02-29 15:39:32 +0100

    fix

--- cmd/snd-cli/main.go
+++ cmd/snd-cli/main.go
@@ -11,7 +11,7 @@ var version = "v0.0.0"
 func main() {
 	rootCmd, _ := root.NewCmdRoot()
 	rootCmd.Version = version
-	rootCmd.SetVersionTemplate(fmt.Sprintf("Version of snd-cli is: %s", version))
+	rootCmd.SetVersionTemplate(fmt.Sprintf("snd-cli version %s", version))
 	err := rootCmd.Execute()
 	if err != nil {
 		fmt.Println("Error: ", err)
