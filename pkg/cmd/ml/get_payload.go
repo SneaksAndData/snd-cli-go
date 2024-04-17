2eadd353a396d36a23ba63f23f9f14c76dd8a7a8
adelinag08 <adma@ecco.com>
2024-04-17 15:32:12 +0200

    Get crystal payload

---
+++ pkg/cmd/ml/get_payload.go
@@ -0,0 +1,51 @@
+package ml
+
+import (
+	"fmt"
+	algorithmClient "github.com/SneaksAndData/esd-services-api-client-go/algorithm"
+	"github.com/spf13/cobra"
+	"snd-cli/pkg/cmdutil"
+	"strings"
+)
+
+func NewCmdGetPayload(authServiceFactory *cmdutil.AuthServiceFactory, serviceFactory cmdutil.ServiceFactory) *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "payload",
+		Short: "Get the payload for a ML Algorithm run",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			authService, err := cmdutil.InitializeAuthService(url, env, authProvider, *authServiceFactory)
+			if err != nil {
+				return err
+			}
+			service, err := serviceFactory.CreateService("algorithm", env, url, authService)
+			if err != nil {
+				return err
+			}
+			resp, err := getRun(service.(*algorithmClient.Service), id, algorithm)
+			if err == nil {
+				fmt.Println(resp)
+			}
+			return err
+		},
+	}
+	cmd.Flags().StringVarP(&id, "id", "i", "", "Specify the Crystal Job ID")
+	err := cmd.MarkFlagRequired("id")
+	if err != nil {
+		fmt.Println("failed to mark 'id' as a required flag: %w", err)
+		return nil
+	}
+	return cmd
+}
+
+func getPayloadRun(algorithmService Service, id, algorithm string) (string, error) {
+	response, err := algorithmService.RetrievePayloadUri(id, algorithm)
+	if err != nil {
+		if strings.HasSuffix(err.Error(), "404") {
+			return "", fmt.Errorf("failed to find run for algorithm %s with run id %s : %v", algorithm, id, "Run not found")
+		}
+		return "", fmt.Errorf("failed to retrieve run for algorithm %s with run id %s: %w", algorithm, id, err)
+	}
+	fmt.Println(response.PayloadUri)
+
+	return response, nil
+}
